326c28c2bba7fd2b988ab03623db15f6efd00ad7
alicika <aaajiiiii@outlook.jp>
2020-05-16 06:10:21 +0900

    Function that denotes a hierarchical distribution over a range of scores with C programming language.

---
+++ byval.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+double power (double x, int y)
+{
+    int i;
+    double tmp = 1.0;
+
+    for (i = 0; i <= n; i++)
+        tmp *= x;
+    return tmp;
+
+}
+
+int main(void)
+{
+    double a;
+    int b;
+
+    printf("calc the first arg powered by the second\n");
+    printf("real number a: ");  scanf("%lf", %a)
+    printf("real number b:");   scanf("%d", %b);
+
+    printf("%.2f powered by %d is: %.2f \n", a, b, power(a, b));
+
+    return 0;
+}
